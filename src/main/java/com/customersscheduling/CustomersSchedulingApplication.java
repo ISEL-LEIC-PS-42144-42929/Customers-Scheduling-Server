f907a20c5a0d7d819c2bb80d7baf97baa0385345
BitoEngineer <bito_asd@hotmail.com>
2018-04-08 14:15:45 +0100

    Project Created

---
+++ src/main/java/com/customersscheduling/CustomersSchedulingApplication.java
@@ -0,0 +1,12 @@
+package com.customersscheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersSchedulingApplication.class, args);
+	}
+}
