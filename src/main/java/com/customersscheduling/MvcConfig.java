77aea8fd2bb05ffb4b109cfbc86145b864021cf3
BitoEngineer <bito_asd@hotmail.com>
2018-07-07 20:21:36 +0100

    HTTP Cache DONE & Began of Database caching

--- src/main/java/com/customersscheduling/MvcConfig.java
+++ src/main/java/com/customersscheduling/MvcConfig.java
@@ -5,6 +5,8 @@ import com.customersscheduling.Service.IBookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -12,14 +14,18 @@ import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import javax.servlet.Filter;
 
 import static org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType.HAL;
 
 //@EnableHypermediaSupport(type = HAL)
 @Configuration
 @EnableScheduling
+@EnableCaching
 @EnableTransactionManagement
 @ComponentScan({"com.customersscheduling.Controller","com.customersscheduling.Service", "com.customersscheduling.ExceptionHandling"})
 public class MvcConfig extends WebMvcConfigurationSupport {
@@ -31,6 +37,11 @@ public class MvcConfig extends WebMvcConfigurationSupport {
     @Scheduled(fixedRate = 5000/*1000 * 60 * 60 * 24*/) //daily rate
     public void dailyBookingUpdate() {
         //bookingService.dailyUpdate();
+    }
+
+    @Bean
+    public Filter etagFilter(){
+        return new ShallowEtagHeaderFilter();
     }
 
 
